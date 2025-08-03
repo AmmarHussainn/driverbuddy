9aa330c310cd0c39728b0b63807507eb68acaddf
Ammar <ammarhussain0315@gmail.com>
2025-08-03 20:54:25 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 
-import Home from './pages/home';
+import Home from './pages/Home';
 import ContactUsPage from './pages/ContactUsPage';
 import PricingPage from './pages/PricingPage';
 
